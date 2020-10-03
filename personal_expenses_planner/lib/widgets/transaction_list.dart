d376a9095cd9fc50f2be266d22645c3b42138c2b
margaiwangara <margaiwangara@gmail.com>
2020-10-04 00:25:13 +0300

    Flutter Udemy Course #2 Complete

--- personal_expenses_planner/lib/widgets/transaction_list.dart
+++ personal_expenses_planner/lib/widgets/transaction_list.dart
@@ -11,7 +11,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400.0,
+      height: 350.0,
       child: transactions.isEmpty
           ? Column(
               children: <Widget>[
